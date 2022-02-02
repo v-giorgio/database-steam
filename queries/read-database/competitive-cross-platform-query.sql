386f38a6097aa66507c3a5c8350b0775a2a28646
v-giorgio <vitorlcardoso17@gmail.com>
2022-02-02 13:54:30 -0300

    update: alterações em algumas queries de consulta

--- queries/read-database/competitive-cross-platform-query.sql
+++ queries/read-database/competitive-cross-platform-query.sql
@@ -1,16 +1,5 @@
-USE steam_store;
-
-/* quantidade de jogos no banco */
-SELECT COUNT(appid) AS `QUANTIDADE DE JOGOS` FROM steam;
-
-/* quantidade de jogos que possuem a categoria 'cross-platform' e a tag 'competitive' */
-SELECT COUNT(A.name) AS `QUANTIDADE DE JOGOS DA CONSULTA` FROM steam AS A
-INNER JOIN steamspy_tag_data AS B
-ON A.appid = B.appid
-WHERE A.categories LIKE '%cross-platform%'  && B.competitive > 0;
-
-/* nome e preço do(s) jogo(s) que se encaixam na consulta anterior */
-SELECT A.name AS `NOME DO JOGO`, A.price AS `PREÇO DO JOGO` FROM steam AS A
+/* quantidade de jogos (e seus nomes e preços) que possuem a categoria 'cross-platform' e a tag 'competitive' */
+SELECT A.name AS `JOGO`, CONCAT('Preço: $ ', A.price) AS `PREÇO DO JOGO`, COUNT(A.name) AS `QUANTIDADE DE JOGOS DA CONSULTA` FROM steam AS A
 INNER JOIN steamspy_tag_data AS B
 ON A.appid = B.appid
 WHERE A.categories LIKE '%cross-platform%'  && B.competitive > 0;
--- queries/read-database/price-positive-ratings-query.sql
+++ queries/read-database/price-positive-ratings-query.sql
@@ -1,24 +1,11 @@
-USE steam_store;
-
-/* quantidade de jogos no banco */
-SELECT COUNT(appid) AS `QUANTIDADE DE JOGOS` FROM steam;
-
-/* nomes e preços dos jogos que se encaixam na consulta */
-SELECT name AS `NOME DO JOGO`, positive_ratings AS `AVALIAÇÕES POSITIVAS`, price AS `PREÇO`,
+/* quantidade de jogos pelo preço menor que 4 reais com avaliações positivas acima da média */
+SELECT COUNT(appid) AS `QUANTIDADE DE JOGOS POR MENOS DE 4 DÓLARES`,
 CASE
-	WHEN price > 0
-		THEN 'Pago'
+	WHEN positive_ratings > (SELECT AVG(positive_ratings) FROM steam)
+		THEN 'Acima da média'
 	WHEN price = 0
-		THEN 'Gratuito'
-	ELSE 'ERRO'
-END AS `VALOR`
+		THEN 'Abaixo da média'
+	ELSE 'Abaixo da média'
+END AS `AVALIAÇÕES POSITIVAS`
 FROM steam
-WHERE price < 4 &&
-positive_ratings > (SELECT AVG(positive_ratings) FROM steam);
-
-/* quantidade de jogos que se encaixam na consulta */
-SELECT COUNT(appid) AS `QUANTIDADE DE JOGOS DA CONSULTA`
-FROM steam
-WHERE price < 4 &&
-positive_ratings > (SELECT AVG(positive_ratings) FROM steam);
-
+WHERE price < 4 GROUP BY `AVALIAÇÕES POSITIVAS`;
